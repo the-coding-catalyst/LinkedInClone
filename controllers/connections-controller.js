3d2d76dd5297c4eceda68722d82e54adde4df47f
the-coding-catalyst <thecodingcatlyst@gmail.com>
2023-01-04 11:54:01 +0530

    Search connections feature

--- controllers/connections-controller.js
+++ controllers/connections-controller.js
@@ -22,6 +22,8 @@ const ifRequestAlreadySent = async(firstUser, secondUser) => {
         console.log(err)
     }
 }
+
+
 const seeConnectionRequests = async (req, res, next) => {
     let email = loggedInUserInfo(req)
     let user = await User.findOne({email: email})
@@ -85,4 +87,64 @@ const acceptConnectionRequest = async (req, res, next) => {
     return res.status(200).json({message: "Connection Request accepted.."})
 }
 
-module.exports = {sendConnectionRequest, acceptConnectionRequest, seeConnectionRequests, seeConnectionsList}
+const breadthFirstSearch = async (users, usersInfo, connections, currentUser) => {
+    const emails = []
+    const queue = []
+    const seen = []
+    seen.push(currentUser.email)
+    users.forEach((user) => {
+        emails.push(user.email)
+    })
+    connections.forEach((friend) => {
+        queue.push({"email": friend, "level": 1})
+    })
+    // let current = queue.pop(0)
+    // console.log(current)
+    // console.log(queue)
+    while(queue.length > 0){
+        let current = queue.pop(0)
+        // console.log("this is current", current)
+        seen.push(current.email)
+        // console.log("users info", users)
+        if(emails.includes(current.email)){
+            let currentLevel = usersInfo[current.email]["connection"]
+            if(currentLevel>current.level){
+                usersInfo[current.email]["connection"] = current.level
+            }
+            
+            console.log(current.email, current.level)
+        }
+        // console.log(current, "current info")
+        let currentUser = await User.findOne({email: current.email})
+        let newConnections = currentUser.connections
+        newConnections.forEach((newUser) => {
+            if(!seen.includes(newUser)) {
+                queue.push({"email": newUser, "level": current.level+1})
+            }
+        })
+    } 
+    return usersInfo
+
+}
+
+const searchUser = async(req, res, next) => {
+    let name = req.body.name
+    let users = await User.find({name})
+    let currentUserEmail = loggedInUserInfo(req)
+    let currentUser = await User.findOne({email: currentUserEmail})
+    let connections = currentUser.connections
+    const usersInfo = {}
+    users.forEach((user)=>{
+        usersInfo[user.email] = ({"name": user.name, "email": user.email, "connection": 3})
+    })
+    const result = await breadthFirstSearch(users, usersInfo, connections, currentUser)
+    // console.log(usersInfo.filter("email" == "ramit.cs18@nitp.ac"))
+    return res.status(200).json({result})
+}
+
+module.exports = {
+    sendConnectionRequest,
+    acceptConnectionRequest, 
+    seeConnectionRequests, 
+    seeConnectionsList,
+    searchUser}
